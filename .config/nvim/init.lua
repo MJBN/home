582155c42a26ac0fabf672f1bd695b19c9fdf58c
Mohammad Javad Bagheri Nejad <git@mjbn.ir>
2022-07-07 01:51:18 +0430

    changing back nvim to one file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,36 @@
-require('lib.setting')
-require("lib.plugin")
-require("lib.keymap")
+--Setting
+vim.opt.encoding = "utf-8"
+vim.opt.nu = true
+vim.opt.wrap = false
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 8
+vim.opt.termguicolors = true
+vim.opt.expandtab = true
+vim.g.mapleader = " "
+ 
+--VimCMD  
+vim.cmd("filetype plugin indent on")         
+vim.cmd("syntax enable")
+
+--Vim-Plug
+local Plug = vim.fn['plug#']  
+vim.call( "plug#begin", "~/.local/share/nvim/plugged")
+    Plug('jiangmiao/auto-pairs')
+    Plug('ap/vim-css-color')     
+    Plug('neoclide/coc.nvim', {['branch'] = 'release'})
+    Plug('dracula/vim', { ['as']= 'dracula' })
+    Plug('airblade/vim-gitgutter')
+vim.call( "plug#end")     
+
+--VimCMD                        
+vim.cmd("colorscheme dracula")
+vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+
+--Shortcut
+vim.keymap.set('n','<Leader>w', ':w<CR>')
+vim.keymap.set('n','<Leader>q', ':q<CR>')
+vim.keymap.set('n','<Leader>wq', ':wq<CR>')
+
--- .config/nvim/lib/keymap.lua
+++
@@ -1,4 +0,0 @@
---Shortcut
-vim.keymap.set('n','<Leader>w', ':w<CR>')
-vim.keymap.set('n','<Leader>q', ':q<CR>')
-vim.keymap.set('n','<Leader>wq', ':wq<CR>')
